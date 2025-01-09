64587647a4c33e388531ab6baea0a63098079315
guillermodotn <guillerm0.n@outlook.es>
2025-01-09 14:28:34 +0100

    Home screen placeholder & screen manager.

--- deepbreathe/app.py
+++ deepbreathe/app.py
@@ -3,7 +3,10 @@ kivy.require('2.3.1') # replace with your current kivy version !
 
 from kivymd.app import MDApp
 
+from kivymd.uix.screenmanager import MDScreenManager
+
 from deepbreathe.widgets.custom_button import CustomButton
+from deepbreathe.screens.home_screen import HomeScreen
 
 class DeepBreathe(MDApp):
 
---
+++ deepbreathe/screens/home_screen.py
@@ -0,0 +1,12 @@
+from kivymd.uix.screen import MDScreen
+
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+
+from kivy.lang import Builder
+
+# Explicitly load the .kv file
+Builder.load_file('deepbreathe/screens/homescreen.kv')
+
+class HomeScreen(MDScreen):
+    pass
