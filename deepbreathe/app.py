260ba53ff968b739f965cdbc8f892baffb6cb656
guillermodotn <guillerm0.n@outlook.es>
2025-01-09 15:23:47 +0100

    Screen switching functionality.

--- deepbreathe/app.py
+++ deepbreathe/app.py
@@ -6,13 +6,23 @@ from kivymd.app import MDApp
 from kivymd.uix.navigationdrawer import MDNavigationLayout
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.navigationdrawer import MDNavigationDrawer
-from kivymd.uix.navigationdrawer import MDNavigationDrawerLabel
+from kivymd.uix.navigationdrawer import MDNavigationDrawerItem
+from kivymd.uix.navigationdrawer import MDNavigationDrawerDivider
+from kivymd.uix.navigationdrawer import MDNavigationDrawerMenu
 
 from deepbreathe.widgets.custom_button import CustomButton
 from deepbreathe.screens.home_screen import HomeScreen
+from deepbreathe.screens.co2_screen import CO2Screen
+from deepbreathe.screens.o2_screen import O2Screen
 
 class DeepBreathe(MDApp):
 
     def build(self):
         pass
+    
+    def change_screen(self, screen_name):
+        # Switch to the screen passed as the argument
+        self.root.ids.screen_manager.current = screen_name
+        # Close the navigation drawer when a menu item is clicked
+        self.root.ids.nav_drawer.set_state("close")
 
