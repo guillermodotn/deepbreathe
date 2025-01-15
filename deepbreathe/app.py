33a7e595833a833e1065c3acad8e24234c237b18
guillermodotn <guillerm0.n@outlook.es>
2025-01-15 19:18:35 +0100

    O2 screen.

--- deepbreathe/app.py
+++ deepbreathe/app.py
@@ -26,8 +26,9 @@ class DeepBreathe(MDApp):
     def build(self):
         pass
     
-    def change_screen(self, screen_name, title):
-        
+    def change_screen(self, screen_name, title, color = (1, 0, 1, 1)):
+        self.root.ids.appbar.md_bg_color = color
+        # Change title of the appbar
         self.root.ids.appbar.ids.title.text = title
         # Switch to the screen passed as the argument
         self.root.ids.screen_manager.current = screen_name
--- deepbreathe/screens/o2_screen.py
+++ deepbreathe/screens/o2_screen.py
@@ -1,22 +1,42 @@
 from kivymd.uix.screen import MDScreen
-
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDButton, MDButtonText
 from kivymd.uix.label import MDLabel
+
+from deepbreathe.widgets.progress_circle import ProgressCircle
 
 from kivy.lang import Builder
 
 # Explicitly load the .kv string
-Builder.load_string("""
-<O2Screen>:
-    MDBoxLayout:
-        orientation: "vertical"
-
-        MDLabel:
-            text: "O2 Screen"
-            halign: "center"
-            text_color: "white"
-""")
+# Builder.load_string("""
+# <O2Screen>:
+#     MDBoxLayout:
+#         orientation: "vertical"
+# """)
 
 
 class O2Screen(MDScreen):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        root_layout = MDBoxLayout(orientation='vertical')
+
+        self.progress_circle = ProgressCircle()
+        self.progress_circle.size_hint = (None, None)
+        self.progress_circle.size = (400, 400)
+        self.progress_circle.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        button = MDButton(MDButtonText(text="Update Progress"))
+        button.bind(on_release=self.update_progress)
+        button.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+
+        root_layout.add_widget(self.progress_circle)
+        root_layout.add_widget(button)
+        self.add_widget(root_layout)
+        # return root
+    
+    def update_progress(self, *args):
+        from random import randint
+        # Randomly set a new progress value
+        new_progress = randint(10, 100)
+        self.progress_circle.set_progress(new_progress)
